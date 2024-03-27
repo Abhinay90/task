4756f3af7c71522fd4d259708d65466cf17e5344
abhinay90 <pratapsinghabhinay17@gmail.com>
2024-03-27 18:19:27 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import axios from 'axios';
+import './App.css'
+import Card from './Card'
+import Temp from './Temp'
+import { useState,useEffect } from 'react'; 
+function App() {
+  return (<>
+        {/* {currentActivity.key} */}
+        <Card></Card>
+        {/* <Temp></Temp> */}
+  </>
+      )
+}
+
+export default App
